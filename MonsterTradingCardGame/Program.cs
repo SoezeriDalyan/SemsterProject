ff07a80a5856777751e25562ec35a244c2b2e0ee
DalyanSoezeri <if23b053@technikum-wien.at>
2023-11-08 00:34:58 +0100

    Creation of server with plain text dummy response

--- MonsterTradingCardGame/Program.cs
+++ MonsterTradingCardGame/Program.cs
@@ -8,6 +8,38 @@ class Program
 {
     static async Task Main()
     {
-        await Console.Out.WriteLineAsync("Hello World");
+        const int port = 10001;
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Listening on Address localhost...");
+        Console.WriteLine($"Listening on port {port}...");
+
+        while (true)
+        {
+            using (var client = await listener.AcceptTcpClientAsync())
+            using (var networkStream = client.GetStream())//Create Stream, to send and recieve Data
+            {
+                var requestBytes = new byte[1024]; //Set byte length, TCP cuts Packest at 1024 bytes
+                await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                var request = Encoding.UTF8.GetString(requestBytes);
+
+                //heandle user createn or login request
+                if (request.StartsWith("GET /users HTTP/1.1"))
+                {
+                    //GET User, read User from (later) Postgres
+                    string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";// for now plain Text, later change to json
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                }
+                else if (request.StartsWith("POST /users HTTP/1.1"))
+                {
+                    //POST User, save User into (later) Postgres | ado.net 
+                    string responseData = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nPosted data received!";// for now plain Text, later change to json
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(responseData);
+                    Console.WriteLine(request);
+                    await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                }
+            }
+        }
     }
 }
